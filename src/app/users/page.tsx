6ac981568ca4bbad094ac6d8ab6a7d60e1682ded
Alemol <alemolamg@outlook.com>
2024-07-31 13:52:34 +0200

    work on users

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,12 +3,9 @@
 import React, { useState, useEffect } from "react";
 import { User } from "@/models/User";
 import UserTable from "@/components/UserTable";
-import Modal from "@/components/Modal";
-import AddUserForm from "@/components/AddUserForm";
 
 const UsersPage: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -35,21 +32,7 @@ const UsersPage: React.FC = () => {
 
   return (
     <div>
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Añadir Usuario
-      </button>
-
-      <UserTable users={users} onUpdateUser={() => {}} />
-
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <AddUserForm
-          onSubmit={handleAddUser}
-          onClose={() => setIsModalOpen(false)}
-        />
-      </Modal>
+      <UserTable users={users} onUpdateUser={handleAddUser} />
     </div>
   );
 };
--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -2,6 +2,9 @@
 
 import React, { useState, useEffect } from "react";
 import { User } from "@/models/User";
+import Modal from "@/components/Modal";
+import AddUserForm from "@/components/AddUserForm";
+import { UserPlusIcon } from "@heroicons/react/16/solid";
 
 interface UserTableProps {
   users: User[];
@@ -11,6 +14,7 @@ interface UserTableProps {
 const UserTable: React.FC<UserTableProps> = ({ users, onUpdateUser }) => {
   const [filterName, setFilterName] = useState("");
   const [filteredUsers, setFilteredUsers] = useState<User[]>(users);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     let filtered = users;
@@ -24,8 +28,28 @@ const UserTable: React.FC<UserTableProps> = ({ users, onUpdateUser }) => {
     setFilteredUsers(filtered);
   }, [filterName, users]);
 
+  const handleAddUser = async (newUser: Omit<User, "id">) => {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    const addedUser = await response.json();
+    // setUsers((prevUsers) => [...prevUsers, addedUser]);
+  };
+
   return (
     <div className="p-4">
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <AddUserForm
+          onSubmit={handleAddUser}
+          onClose={() => setIsModalOpen(false)}
+        />
+      </Modal>
+      <h1 className="text-2xl font-bold mb-4">Usuarios</h1>
       <div className="flex justify-between items-center mb-4">
         <div>
           <input
@@ -36,9 +60,15 @@ const UserTable: React.FC<UserTableProps> = ({ users, onUpdateUser }) => {
             className="border p-2 rounded"
           />
         </div>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="flex justify-center bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          <UserPlusIcon className="h-5 w-5 mr-1" />
+          Añadir Usuario
+        </button>
       </div>
 
-      <h1 className="text-2xl font-bold mb-4">Usuarios</h1>
       <table className="min-w-full border-collapse block md:table">
         <thead className="block md:table-header-group">
           <tr className="border-b border-gray-200 md:table-row">
